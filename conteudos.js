16704c4b105f3eca296c76126b80532bc016cac0
GustavoPatussi <gustavopatussibr@gmail.com>
2022-07-30 15:38:50 -0300

    3 files modified, 1 added

---
+++ conteudos.js
@@ -0,0 +1,27 @@
+const conteudo = [
+  {
+    titulo: 'cultura Digital',
+  },
+  {
+    titulo: 'Matemática',
+  },
+  {
+    titulo: 'Língua Portuguesa',
+  },
+  {
+    titulo: 'Geografia',
+  },
+  {
+    titulo: 'História',
+  },
+  {
+    titulo: 'Física',
+  },
+  {
+    titulo: 'Química',
+  },
+];
+
+function checkDisciplina(i){
+  loadConteudo(conteudo[i]);
+}
--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const conteudo = [
-  {
-    titulo: 'cultura Digital',
-  },
-];
-
-function Menuzain() {
+function menuDisplay() {
   const lateral = document.getElementById('menu_lateral');
   if (lateral.style.visibility == '') {
     lateral.style.visibility = 'visible';
@@ -14,12 +8,20 @@ function Menuzain() {
     lateral.style.visibility = 'hidden';
   }
 }
-function Menuzain2() {
+
+function menuDisplay2() {
   const lateral = document.getElementById('menu_lateral');
   if (lateral.style.visibility == 'visible') {
     lateral.style.visibility = 'hidden';
   }
 }
-function LoadConteudo() {
-  var divConteudo = document.getElementById('conteudo');
+
+function loadConteudo(elemento) {
+  var divConteudo = document.getElementById('Conteudo');
+  divConteudo.innerHTML = '';
+  var txtTitulo = document.createElement('h1');
+  txtTitulo.setAttribute('class', 'titulo');
+  txtTitulo.innerText = elemento.titulo;
+  
+  divConteudo.appendChild(txtTitulo);
 }
