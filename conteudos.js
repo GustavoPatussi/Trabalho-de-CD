28177d22731ce0c8d158c38ecda5e30acccff9ef
GustavoPatussi <gustavopatussibr@gmail.com>
2022-08-03 23:17:45 -0300

    1 file modified

--- conteudos.js
+++ conteudos.js
@@ -1,30 +1,38 @@
 const conteudo = [
   {
     titulo: 'cultura Digital',
+    resumo: '',
   },
   {
     titulo: 'Matemática',
+    resumo: '',
   },
   {
     titulo: 'Língua Portuguesa',
+    resumo: '',
   },
   {
     titulo: 'Geografia',
+    resumo: '',
   },
   {
     titulo: 'História',
+    resumo: '',
   },
   {
     titulo: 'Física',
+    resumo: '',
   },
   {
     titulo: 'Química',
+    resumo: '',
   },
   {
     titulo: 'Redes e Protocolos',
+    resumo: '',
   },
 ];
 
-function checkDisciplina(i){
+function checkDisciplina(i) {
   loadConteudo(conteudo[i]);
 }
