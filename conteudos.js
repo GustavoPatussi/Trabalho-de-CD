d3f16b081787d8cd6693c107c4d1b48810c5c7f1
GustavoPatussi <gustavopatussibr@gmail.com>
2022-08-05 17:38:21 -0300

    3 files modified

--- conteudos.js
+++ conteudos.js
@@ -5,33 +5,33 @@ const conteudo = [
     links: [
       {
         l: 'https://cdn.jsdelivr.net/gh/GustavoPatussi/Trabalho-de-CD@main/Contents/AULA%2002%20-%20SISTEMAS%20OPERACIONAIS.pdf',
-        n: 'sistemas Operacionais',
+        n: 'sistemas Operacionais (PDF)',
       },
     ],
   },
   {
     titulo: 'Matemática',
-    resumo: '',
+    resumo: 'A matemática estuda quantidades, medidas, espaços, estruturas e variações. Um trabalho matemático consiste em procurar por padrões, formular conjecturas e, por meio de deduções rigorosas a partir de axiomas e definições, estabelecer novos resultados.',
   },
   {
     titulo: 'Língua Portuguesa',
-    resumo: '',
+    resumo: 'Dentre as áreas mais importantes da disciplina, estão: morfologia, que estuda as palavras isoladamente; sintaxe, responsável por estudar as palavras enquanto elementos de uma frase; e semântica, que estuda o significado das palavras e os sentidos que elas podem tomar de acordo com o contexto.',
   },
   {
     titulo: 'Geografia',
-    resumo: '',
+    resumo: 'A Geografia é a ciência que estuda elementos físicos, biológicos e humanos, e suas relações com o planeta Terra. Tem como principal objetivo compreender a relação do homem com o meio ambiente, as condições climáticas e as características do espaço geográfico.',
   },
   {
     titulo: 'História',
-    resumo: '',
+    resumo: 'É chamado de história o conhecimento que estuda as ocorrências do passado. Os historiadores definem essa ciência de alguns modos distintos. Há uma linha de pensamento que acredita que a história estuda, na realidade, a ação dos seres humanos através do tempo, investigando seus comportamentos e suas consequências.',
   },
   {
     titulo: 'Física',
-    resumo: '',
+    resumo: 'Física (do grego antigo: φύσις physis "natureza") é a ciência que estuda a natureza e seus fenômenos em seus aspectos mais gerais. Analisa suas relações e propriedades, além de descrever e explicar a maior parte de suas consequências.',
   },
   {
     titulo: 'Química',
-    resumo: '',
+    resumo: 'A química geral engloba as diferentes áreas do estudo dos compostos químicos, moléculas e substâncias, seja em um nível macroscópico ou microscópico, em termos de composição dos átomos, da mistura de substâncias diferentes, arranjo dos elementos químicos ou outros temas.',
   },
   {
     titulo: 'Redes e Protocolos',
--- script.js
+++ script.js
@@ -18,17 +18,23 @@ function menuDisplay2() {
 
 function loadConteudo(elemento) {
   var divConteudo = document.getElementById('Conteudo');
+  divConteudo.setAttribute('class', 'divConteudo')
   divConteudo.innerHTML = '';
 
   var txtTitulo = document.createElement('h1');
   txtTitulo.setAttribute('class', 'titulo');
   txtTitulo.innerText = elemento.titulo;
-
   divConteudo.appendChild(txtTitulo);
+
+  var txtResumo = document.createElement('p')
+  txtResumo.innerText = elemento.resumo;
+  divConteudo.appendChild(txtResumo);
+
   if (elemento.links != undefined){
     var links = document.createElement('a');
     for (var i = 0; i < elemento.links.length; i++){
       links.innerText = elemento.links[i].n;
+      links.setAttribute('class', 'links')
       links.setAttribute('href', elemento.links[i].l);
       divConteudo.appendChild(links);
     }
